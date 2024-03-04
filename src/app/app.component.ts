d20e3e193134b4dcd155905832193b893d496713
Maggy Zhou <maggyzhou@Maggys-Laptop-Pro.local>
2024-03-04 17:39:51 +0000

    fix: resolve conflict 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProfilePictureComponent } from './profile-picture/profile-picture.component';
-
+import { StoryBannerComponent } from './story-banner/story-banner.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProfilePictureComponent],
+  imports: [RouterOutlet, ProfilePictureComponent, StoryBannerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
