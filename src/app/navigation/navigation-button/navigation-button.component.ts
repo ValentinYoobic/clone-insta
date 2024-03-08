95f045ee08062d56dc2a0eb5f769211a70e7fd81
Valentin Gerard <valentingerard@admins-MacBook-Pro.local>
2024-03-08 17:08:41 +0000

    interactive navbar

--- src/app/navigation/navigation-button/navigation-button.component.ts
+++ src/app/navigation/navigation-button/navigation-button.component.ts
@@ -1,16 +1,20 @@
 import { Component, Input } from '@angular/core';
 import { IconComponent, IconType } from '../../general-components/icon/icon.component';
+import { ProfilePictureComponent } from '../../general-components/profile-picture/profile-picture.component';
+
+type NavigationIconType = IconType | 'Profile'
 
 @Component({
   selector: 'app-navigation-button',
   standalone: true,
-  imports: [IconComponent],
+  imports: [IconComponent, ProfilePictureComponent],
   templateUrl: './navigation-button.component.html',
   styleUrl: './navigation-button.component.sass'
 })
 export class NavigationButtonComponent {
   @Input() label: string = ''
-  @Input() iconType!: IconType;
+  @Input() iconType!: NavigationIconType;
   @Input() active: boolean = false;
-
+  @Input() userProfilePhotoUrl!: string;
+  @Input() handleClick: (event: MouseEvent) => void = (event: MouseEvent) => {console.log('you clicked on the button')}
 }
